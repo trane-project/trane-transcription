56245b89bc26bbd910fc5d667fd48eb169cc6c39
Martin Martinez Rivera <mrtnz.rvr@gmail.com>
2024-07-20 18:50:36 -0700

    Fix courses

--- src/main.rs
+++ src/main.rs
@@ -3,15 +3,12 @@ fn main() {}
 #[cfg(test)]
 mod test {
     use anyhow::Result;
-    use std::{fs, path::Path};
     use trane::{course_library::CourseLibrary, Trane};
 
     #[test]
     fn verify_courses() -> Result<()> {
-        let courses_path = Path::new("courses");
-        let trane = Trane::new(&std::env::current_dir()?, Path::new("courses"))?;
-        assert!(trane.get_all_exercise_ids()?.len() > 0);
-        fs::remove_dir_all(courses_path.join(".trane"))?;
+        let trane = Trane::new_local(&std::env::current_dir()?, &std::env::current_dir()?)?;
+        assert!(trane.get_all_exercise_ids(None).len() > 0);
         Ok(())
     }
 }
